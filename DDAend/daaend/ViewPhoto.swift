9f71cfc9c238d69d02686bec052f66e68ec949fb
junhyungcha <cscoi045@cscoi045ui-iMac.local>
2017-02-08 16:20:11 +0900

    alert scene
    sd

--- DDAend/daaend/ViewPhoto.swift
+++ DDAend/daaend/ViewPhoto.swift
@@ -35,6 +35,7 @@ class ViewPhoto: UIViewController {
             // Add actions for some canned filters.
             alertController.addAction(UIAlertAction(title: NSLocalizedString("Sepia Tone", comment: ""),
                                                     style: .default, handler: nil))
+
             alertController.addAction(UIAlertAction(title: NSLocalizedString("Chrome", comment: ""),
                                                     style: .default, handler: nil))
             
@@ -47,33 +48,7 @@ class ViewPhoto: UIViewController {
 
         
         
-//        guard let image = self.bigPhoto.image?.cgImage else { return }
-//        //이게 뭐냐면 가드렛안되면 그냥 브레이크하는것임
-//        // nil 이 아니면 다음 문단으로 넘어가라
-//        // ui 가 아니라 cgimage로 불러왔다
-//
-//        let openGLContext = EAGLContext(api: .openGLES3)
-//        // open geo context // gpu에 접근하는 것. 필터 매우 복잡한 작업이기 때문에
-//        let context = CIContext(eaglContext: openGLContext!)
-//        // open image context // core image와 gpu를 잇는 conjunction
-//        
-//        let ciImage = CIImage(cgImage: image)
-//        // create a core image
-//        let filter = CIFilter(name: "CISepiaTone")
-//        // create a filter : we want to use
-//        
-//        filter?.setValue(ciImage, forKey: kCIInputImageKey) // 필터가 적용될 이미지로 ciimage를 넣어라
-//        filter?.setValue(1, forKey: kCIInputIntensityKey) // 필터의 적용 강도를 1로 정해라
-//        
-//        
-//        /* if let sepiaOutput = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
-//         let exposureFilter = CIFilter(name: "CIExposureAdjust")
-//         exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
-//         exposureFilter?.setValue(1, forKey: kCIInputEVKey) */
-//        
-//        if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage{
-//            self.bigPhoto?.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
-//        }
+
         
         
     }
@@ -101,4 +76,36 @@ class ViewPhoto: UIViewController {
     }
     */
 
+    func filterPhoto(_: UIAlertAction) -> (){
+                guard let image = self.bigPhoto.image?.cgImage else { return }
+                //이게 뭐냐면 가드렛안되면 그냥 브레이크하는것임
+                // nil 이 아니면 다음 문단으로 넘어가라
+                // ui 가 아니라 cgimage로 불러왔다
+        
+                let openGLContext = EAGLContext(api: .openGLES3)
+                // open geo context // gpu에 접근하는 것. 필터 매우 복잡한 작업이기 때문에
+                let context = CIContext(eaglContext: openGLContext!)
+                // open image context // core image와 gpu를 잇는 conjunction
+        
+                let ciImage = CIImage(cgImage: image)
+                // create a core image
+                let filter = CIFilter(name: "CISepiaTone")
+                // create a filter : we want to use
+        
+                filter?.setValue(ciImage, forKey: kCIInputImageKey) // 필터가 적용될 이미지로 ciimage를 넣어라
+                filter?.setValue(1, forKey: kCIInputIntensityKey) // 필터의 적용 강도를 1로 정해라
+        
+        
+                /* if let sepiaOutput = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+                 let exposureFilter = CIFilter(name: "CIExposureAdjust")
+                 exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
+                 exposureFilter?.setValue(1, forKey: kCIInputEVKey) */
+        
+                if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage{
+                    self.bigPhoto?.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
+                }
+    }
+    
 }
+
+
